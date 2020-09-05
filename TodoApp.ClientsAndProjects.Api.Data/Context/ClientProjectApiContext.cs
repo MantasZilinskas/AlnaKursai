3af8780e0700054e408c75173d8388a3bd7da2ad
Mantas Žilinskas <43217237+MantasZilinskas@users.noreply.github.com>
2020-09-05 17:29:16 +0300

    Added Context class

---
+++ TodoApp.ClientsAndProjects.Api.Data/Context/ClientProjectApiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.ClientsAndProjects.Api.Data.Model;
+
+namespace TodoApp.ClientsAndProjects.Api.Data.Context
+{
+    public class ClientProjectApiContext : DbContext
+    {
+        public ClientProjectApiContext(DbContextOptions<ClientProjectApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
