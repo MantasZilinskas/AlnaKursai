cd6e659ed4fae939b6fe3e749904ba76e1f63d1b
Mantas Žilinskas <43217237+MantasZilinskas@users.noreply.github.com>
2020-09-06 20:07:53 +0300

    Made Project's client nullable

--- TodoApp.ClientsAndProjects.Api.Data/Migrations/20200906164328_NullableClientInProjectEntity.Designer.cs
+++ TodoApp.ClientsAndProjects.Api.Data/Migrations/20200906164328_NullableClientInProjectEntity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TodoApp.ClientsAndProjects.Api.Data.Context;
 
 namespace TodoApp.ClientsAndProjects.Api.Data.Migrations
 {
     [DbContext(typeof(ClientProjectApiContext))]
-    partial class ClientProjectApiContextModelSnapshot : ModelSnapshot
+    [Migration("20200906164328_NullableClientInProjectEntity")]
+    partial class NullableClientInProjectEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TodoApp.ClientsAndProjects.Api.Data/Model/ProjectDAO.cs
+++ TodoApp.ClientsAndProjects.Api.Data/Model/ProjectDAO.cs
@@ -11,7 +11,7 @@ namespace TodoApp.ClientsAndProjects.Api.Data.Model
         [Required]
         public string Name { get; set; }
         public string Description { get; set; }
-        public int ClientId { get; set; }
+        public int? ClientId { get; set; }
         public ClientDAO Client { get; set; }
     }
 }
