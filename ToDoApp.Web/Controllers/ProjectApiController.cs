4357b8953de66ab6cadb5f94f0b0f3bf90829669
Mantas Žilinskas <43217237+MantasZilinskas@users.noreply.github.com>
2020-09-06 19:24:55 +0300

    Filled project api controller with api calls

--- ToDoApp.Web/Controllers/ProjectApiController.cs
+++ ToDoApp.Web/Controllers/ProjectApiController.cs
@@ -4,21 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TodoApp.ClientsAndProjects.ApiClient;
 
 namespace TodoApp.Web.Controllers
 {
     public class ProjectApiController : Controller
     {
-        // GET: ProjectApiController
-        public ActionResult Index()
+        private readonly Client apiClient;
+
+        public ProjectApiController(Client apiClient)
         {
-            return View();
+            this.apiClient = apiClient;
+        }
+
+        // GET: ProjectApiController
+        public async Task<ActionResult> Index()
+        {
+            return View(await apiClient.ApiProjectsGetAsync());
         }
 
         // GET: ProjectApiController/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
         {
-            return View();
+            var project = await apiClient.ApiProjectsGetAsync(id);
+            return View(project);
         }
 
         // GET: ProjectApiController/Create
@@ -30,57 +39,62 @@ namespace TodoApp.Web.Controllers
         // POST: ProjectApiController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(ProjectDAO project)
         {
             try
             {
+                await apiClient.ApiProjectsPostAsync(project);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(project);
             }
         }
 
         // GET: ProjectApiController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int id)
         {
-            return View();
+            var project = await apiClient.ApiProjectsGetAsync(id);
+            return View(project);
         }
 
         // POST: ProjectApiController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(int id, ProjectDAO project)
         {
             try
             {
+                await apiClient.ApiProjectsPutAsync(id,project);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(project);
             }
         }
 
         // GET: ProjectApiController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            return View();
+            var project = await apiClient.ApiProjectsGetAsync(id);
+            return View(project);
         }
 
         // POST: ProjectApiController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(int id, ProjectDAO project)
         {
             try
             {
+                await apiClient.ApiProjectsDeleteAsync(id);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(project);
             }
         }
     }
